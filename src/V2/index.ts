92547c6bce2c4687132ea2f9593b399e10f72a0f
Derrick Timmermans <derrick.timmermans@outlook.com>
2024-01-24 15:33:09 +0100

    Allow users to input websocket address

--- src/V2/index.ts
+++ src/V2/index.ts
@@ -3,12 +3,28 @@ import { ControllerScene } from "./ControllerScene";
 import { EDMOClient } from "../EDMOClient";
 
 const canvas = document.getElementById('renderCanvas') as HTMLCanvasElement;
-canvas.style.height = "100vh"
-canvas.style.width = "100vw"
+canvas.style.height = "100vh";
+canvas.style.width = "100vw";
+
+
+const lastUsedWebsocket = document.cookie.replace(/(?:(?:^|.*;\s*)websocket\s*\=\s*([^;]*).*$)|^.*$/, "$1");
+
+var userInput: string | null;
+while (true) {
+
+    // Ask the user for input using a popup
+    userInput = window.prompt("Please enter the server websocket address", lastUsedWebsocket);
+
+    if (userInput?.startsWith("ws://") || userInput?.startsWith("wss://"))
+        break;
+}
+
+// Cache the input in cookies
+document.cookie = `websocket=${userInput}`;
 
 const engine = new Engine(canvas);
 
-const edmoClient = new EDMOClient("ws://0.0.0.0:8080");
+const edmoClient = new EDMOClient(userInput);
 
 await edmoClient.waitForId(3000);
 
